f3ce2231631e756852e530a4c09e85ddf606f9d6
gourav1995 <gourav.mish1995@gmail.com>
2018-10-17 19:56:46 +0530

    commit

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'datatables.net'
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -10,6 +12,60 @@ export class AdminDashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $(function () {
+        $.ajax({
+          url: 'http://34.213.106.173/api/user/getAdminUserList',
+          type: 'GET',
+          success: function (result) {
+            console.log("success", result);
+            var list = [];
+            for(var i =0;i<result.data.data.length;i++)
+            {
+              list.push([i+1,result.data.data[i].firstName,result.data.data[i].lastName,result.data.data[i].email,result.data.data[i].service])
+            }
+            $('#table1').DataTable({
+              data : list
+            })
+          },
+          error: function (error) {
+            console.log(error);
+          }
+        })
+        return false;
+      })
+    })
+    var token = localStorage.getItem('token');
+    $(document).ready(function () {
+      $(function () {
+        $.ajax({
+          url: 'http://34.213.106.173/api/user/UserStatics',
+          type: 'GET',
+          headers:
+          {
+            'Authorization':token
+          },
+          success: function (result) {
+            console.log("success", result);
+            var user = result.data.details;
+            var html = '';
+            for(var index =0;index<user.length;index++)
+            {
+            html+="<div class='card'>";
+            html+="<div class='card text-black bg-primary mb-3'>"+user[index].service+"</div>";
+            html+="<div class='card-body'>"+user[index].count+"</div>";
+            html+="</div";
+            $("#services").html(html);
+            }
+            
+          },
+          error: function (error) {
+            console.log(error);
+          }
+        })
+        return false;
+      })
+    })
   }
 
 }
--- src/app/components/admin-login/admin-login.component.ts
+++ src/app/components/admin-login/admin-login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-admin-login',
@@ -9,11 +11,53 @@ import { Router } from '@angular/router'
 export class AdminLoginComponent implements OnInit {
 
   constructor(private router: Router) { }
-  // login()
-  // {
-  //   this.router.navigateByUrl('/admin-dashboard');
-  // }
-  ngOnInit() {
-  }
 
+  ngOnInit() {
+    $(document).ready(function () {
+      $('#btnLogin').click(function () {
+        var email=$('#email').val();
+        var password=$('#password').val()
+        var at = email.indexOf("@");
+        var dot = email.indexOf(".");
+        var com = email.indexOf("com");
+      if(email==""){
+        $('#email').focus(); 
+        $('#msg').text("Enter an email id");
+        return false;
+      }
+      else if (password == "" ){
+        $("#password").focus();
+        $('#msgp').text("Enter a password");
+        return false;
+      }
+      else if (at < 1 || (dot - at) < 2 || com < 1){
+        $("#email").focus();
+        $('#msg').text("Enter valid email id");
+        return false;
+  }
+    
+        var data = {
+          "email": $("#email").val(),
+          "password": $("#password").val()
+        }
+        console.log(data);
+        $.ajax({
+          url: 'http://34.213.106.173/api/user/adminLogin',
+          type: 'POST',
+          data: data,
+          success: function (data) {
+            console.log("success", data);
+            localStorage.setItem("token",data.id);
+            window.location.href = "/admin-dashboard";
+          },
+          error: function (error) {
+            console.log(error);
+
+            $("div#errMsg").html("Email or Password Invalid");
+          }
+        })
+        return false;
+      })
+    })
+  }
 }
